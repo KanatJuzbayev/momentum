29dfd8449dc7a0a4f1be38682b9930d9603c7923
Kanat Juzbayev <76990478+KanatJuzbayev@users.noreply.github.com>
2021-10-21 16:55:49 +0600

    feat: add slide background functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const time = document.querySelector('.time');
 const localeDate = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
+const body = document.querySelector('body');
+
+const timeOfDay = [
+  'night',
+  'morning',
+  'afternoon',
+  'evening'
+];
 
 function showTime() {
   const date = new Date();
@@ -22,12 +30,7 @@ function showDate() {
 function getTimeOfDay() {
   const date = new Date();
   const hours = date.getHours();
-  const timeOfDay = [
-    'night',
-    'morning',
-    'day',
-    'evening'
-  ];
+
   greeting.textContent = `Good ${timeOfDay[Math.floor(hours/6)]},`;
 }
 
@@ -41,12 +44,51 @@ window.addEventListener('beforeunload', setLocalStorage);
 
 function getLocalStorage() {
   const name = document.querySelector('.name');
-  if(localStorage.getItem('name')) {
+  if (localStorage.getItem('name')) {
     name.value = localStorage.getItem('name');
   }
 }
 window.addEventListener('load', getLocalStorage);
 
 // background
-const body = document.querySelector(body);
-console.log(body);
+let randomNum = getRandomNum();
+
+function getRandomNum() {
+  let num = Math.floor(Math.random() * (20 - 1) + 1) ;
+  return getImgNum(num);
+}
+
+function getImgNum(num) {
+  let imgNum;
+  if (num < 10) {
+    imgNum = '0' + num;
+  } else {
+    imgNum = num;
+  }
+  return imgNum;
+}
+
+function setBg() {
+  const date = new Date();
+  const hours = date.getHours();
+  body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay[Math.floor(hours/6)]}/${randomNum}.jpg')`;
+}
+
+setBg();
+
+// Slide buttons
+const slideNext = document.querySelector('.slide-next');
+slideNext.addEventListener('click', getSlideNext);
+
+function getSlideNext() {
+  randomNum = (randomNum >= 20) ? randomNum = '01' : getImgNum(parseInt(randomNum) + 1);
+  setBg();
+}
+
+const slidePrev = document.querySelector('.slide-prev');
+slidePrev.addEventListener('click', getSlidePrev);
+
+function getSlidePrev() {
+  randomNum = (randomNum <= 1) ? randomNum = '20' : getImgNum(parseInt(randomNum) - 1);
+  setBg();
+}
